8011748624c7c241950b1ee49d56c5b6fef1b9d0
Demaria Woods <dpwoods@uh.edu>
2019-11-09 23:04:22 -0600

    added production URL

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const AxiosInstance = axios.create({
-     baseURL: 'http://localhost:3000/',
-    // baseURL: 'https://online-italian-restauraunt.herokuapp.com'
+     //baseURL: 'http://localhost:3000/',
+     baseURL: 'https://online-italian-restauraunt.herokuapp.com'
 });
 
 export default AxiosInstance;
