1beb72c376cceef6606f4b5a31729efd65fc690e
Demaria Woods <dwoods447@gmail.com>
2023-06-21 02:11:39 -0500

    check baseURL

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,6 +1,7 @@
 
 import 'dotenv/config'
 import axios from 'axios';
+console.log('baseURL: ' + process.env.baseUR)
 console.log(process.env) // remove this after you've confirmed it is working
 const AxiosInstance = axios.create({
      baseURL: process.env.baseURL || 'http://localhost:3000/'
