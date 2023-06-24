428d8ab061eb26cb298658f2291bc81c7cd6a2b6
Demaria Woods <dwoods447@gmail.com>
2023-06-24 01:49:32 -0500

    remove dotenv

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,10 +1,8 @@
 
 import 'dotenv/config'
 import axios from 'axios';
-console.log('baseURL: ' + process.env.baseUR)
-console.log(process.env) // remove this after you've confirmed it is working
 const AxiosInstance = axios.create({
-     baseURL: process.env.baseURL || 'http://localhost:3000/'
+     baseURL: process.env.VUE_APP_BASE_URL || 'http://localhost:3000/'
 });
 
 export default AxiosInstance;
