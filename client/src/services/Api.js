5acda714047782d0a46e8b551999bec32111239d
Demaria Woods <dwoods447@gmail.com>
2023-06-23 21:05:23 -0500

    remove console statement

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,8 +1,4 @@
 import axios from 'axios';
-// eslint-disable-next-line
-console.log('baseURL: ' + process.env.VUE_APP_CLIENT_BASE)
-// eslint-disable-next-line
-console.log(process.env) // remove this after you've confirmed it is working
 const AxiosInstance = axios.create({
      baseURL: process.env.baseURL || 'http://localhost:3000/'
 });
